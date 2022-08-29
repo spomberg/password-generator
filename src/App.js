c54fd161c8a0dc290fa6f57d0713a2276a2c9a3c
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 18:02:32 -0400

    Import Settings component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import TextField from './components/TextField/TextField';
+import Settings from './components/Settings/Settings';
 
 function App() {
   return (
     <div className='app'>
       <h1>Password Generator</h1>
       <TextField />
+      <Settings />
     </div>
   );
 }
