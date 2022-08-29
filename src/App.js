08b1f5fe04cefc50498f88d11a3e2c2dfd9cb62f
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 16:53:17 -0400

    Add copied status and pass it to textField component as a prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { useState } from 'react';
 import './App.scss';
+import { useState } from 'react';
 import TextField from './components/TextField/TextField';
 
 function App() {
-  const [text, setText] = useState('P4$50rD!');
+  const [text, setText] = useState('P4$5W0rD!');
+  const [copied, setCopied] = useState(false);
 
   return (
     <div className='app'>
       <h1>Password Generator</h1>
-      <TextField text={text}/>
+      <TextField 
+      text={text}
+      copied={copied}
+      setCopied={setCopied}
+      />
     </div>
   );
 }
