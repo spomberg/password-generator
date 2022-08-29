9f6eee0c2baafd5c46fc107acb3077a83c9e671d
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 17:49:11 -0400

    Import copiedReducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import textReducer from './features/text/textSlice';
+import copiedReducer from './features/copied/copiedSlice';
 
 export default configureStore({
   reducer: {
-    text: textReducer
+    text: textReducer,
+    copied: copiedReducer
   },
 });
