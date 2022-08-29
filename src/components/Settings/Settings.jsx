39d6210817479f5d9676ab24394a785aba28d24f
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 18:36:18 -0400

    Import lengthSlice and implement slider

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,7 +1,27 @@
 import './Settings.scss';
+import Slider from '@mui/material/Slider';
+import { useSelector, useDispatch } from 'react-redux';
+import { setLength } from '../../features/length/lengthSlice';
 
 export default function Settings() {
+  const length = useSelector((state) => state.length.value);
+  const dispatch = useDispatch();
+
   return (
-    <div>Settings</div>
+    <div className='settings'>
+      <div className='slider'>
+        <h5>Character Length</h5>
+        <h4>{length}</h4>
+        <Slider 
+          aria-label="Character length"
+          defaultValue={length}
+          valueLabelDisplay="off"
+          step={1}
+          min={0}
+          max={20}
+          onChange={(event, value) => dispatch(setLength(value))}
+        />
+      </div>
+    </div>
   )
 }
