798d0ea4f2250184a90e7cc9c967043edf7bafa4
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 17:23:40 -0400

    Import redux component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './fonts/JetBrainsMono/JetBrainsMono-Italic-VariableFont_wght.ttf';
 import './fonts/JetBrainsMono/JetBrainsMono-VariableFont_wght.ttf';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
