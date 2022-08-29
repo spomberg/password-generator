bc25c090c62590c2997e473494aed8f5097f6673
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-28 22:45:33 -0400

    Implement text state and pass it as a prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import './App.scss';
 import TextField from './components/TextField/TextField';
 
 function App() {
+  const [text, setText] = useState('P4$50rD!');
+
   return (
     <div className='app'>
       <h1>Password Generator</h1>
-      <TextField/>
+      <TextField text={text}/>
     </div>
   );
 }
