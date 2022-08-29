46ab375f99e0fbe163899e81097218f08f792a6d
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 19:10:03 -0400

    Remove lengthReducer

--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import textReducer from './features/text/textSlice';
 import copiedReducer from './features/copied/copiedSlice';
-import lengthReducer from './features/length/lengthSlice';
 
 export default configureStore({
   reducer: {
     text: textReducer,
-    copied: copiedReducer,
-    length: lengthReducer
+    copied: copiedReducer
   },
 });
