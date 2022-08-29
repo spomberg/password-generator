21f575b4b59c9916954e5b9d0cbfa318be3c8d0b
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-28 20:48:10 -0400

    Import font files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './fonts/JetBrainsMono/JetBrainsMono-Italic-VariableFont_wght.ttf';
+import './fonts/JetBrainsMono/JetBrainsMono-VariableFont_wght.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
