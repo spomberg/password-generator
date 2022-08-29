5ae7e3b015700b1b4f5fd4bd7b78ba154ece3d1a
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 18:35:04 -0400

    Add lengthReducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import textReducer from './features/text/textSlice';
 import copiedReducer from './features/copied/copiedSlice';
+import lengthReducer from './features/length/lengthSlice';
 
 export default configureStore({
   reducer: {
     text: textReducer,
-    copied: copiedReducer
+    copied: copiedReducer,
+    length: lengthReducer
   },
 });
