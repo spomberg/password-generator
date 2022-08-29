78c3dabcacc17305abf5772d99a8d89a78bcf853
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 17:48:45 -0400

    Remove copiedState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.scss';
-import { useState } from 'react';
 import TextField from './components/TextField/TextField';
 
 function App() {
-  const [copied, setCopied] = useState(false);
-
   return (
     <div className='app'>
       <h1>Password Generator</h1>
-      <TextField 
-      copied={copied}
-      setCopied={setCopied}
-      />
+      <TextField />
     </div>
   );
 }
