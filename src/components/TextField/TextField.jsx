8d881d8a5447ec46ea51e551572daa299e5d3548
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-28 22:46:45 -0400

    Use text state to display password

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,8 +1,13 @@
 import './TextField.scss';
 import CopyIcon from '../../assets/images/icon-copy.svg';
 
-export default function TextField() {
+export default function TextField(props) {
   return (
-    <div>TextField</div>
+    <div className='text-field'>
+       <span>{props.text}</span>  
+       <button>
+        <img src={CopyIcon} alt="Copy password" />  
+       </button>  
+    </div>
   )
 }
