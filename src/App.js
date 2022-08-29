ddfb27fb0817796668924808554911039b512aec
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 17:32:43 -0400

    Remove text state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { useState } from 'react';
 import TextField from './components/TextField/TextField';
 
 function App() {
-  const [text, setText] = useState('P4$5W0rD!');
   const [copied, setCopied] = useState(false);
 
   return (
     <div className='app'>
       <h1>Password Generator</h1>
       <TextField 
-      text={text}
       copied={copied}
       setCopied={setCopied}
       />
