cbcd0b482a7d63a80a13b47e75e5f0a4a55fc485
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 17:33:16 -0400

    Import and use textSlice and remove textState

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,15 +1,20 @@
 import './TextField.scss';
 import CopyIcon from '../../assets/images/icon-copy.svg';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import { useSelector, useDispatch } from 'react-redux';
+import { setText } from '../../features/text/textSlice';
 
 export default function TextField(props) {
+  const text = useSelector((state) => state.text.value);
+  const dispatch = useDispatch();
+
   return (
     <div className='text-field'>
-       <span {...props.text === 'P4$5W0rD!' && {className: 'defaultText'} }>{props.text}</span>  
+       <span {...text === 'P4$5W0rD!' && {className: 'defaultText'} }>{text}</span>  
         {props.copied && (<span>COPIED</span>)}
         <CopyToClipboard 
           onCopy={() => props.setCopied(true)}
-          text={props.text}
+          text={text}
         >
           <img src={CopyIcon} alt="Copy password" />  
         </CopyToClipboard>
