238b33aa64497d6fe7d4a22e69b6e7daaea13e68
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 16:53:38 -0400

    Import Copy to Clipboard and use it to copy the password

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,13 +1,18 @@
 import './TextField.scss';
 import CopyIcon from '../../assets/images/icon-copy.svg';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 export default function TextField(props) {
   return (
     <div className='text-field'>
-       <span>{props.text}</span>  
-       <button>
-        <img src={CopyIcon} alt="Copy password" />  
-       </button>  
+       <span {...props.text === 'P4$5W0rD!' && {className: 'defaultText'} }>{props.text}</span>  
+        {props.copied && (<span>COPIED</span>)}
+        <CopyToClipboard 
+          onCopy={() => props.setCopied(true)}
+          text={props.text}
+        >
+          <img src={CopyIcon} alt="Copy password" />  
+        </CopyToClipboard>
     </div>
   )
 }
