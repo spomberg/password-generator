71393d6d715a528734761467383197d9ac84e5a3
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 17:49:50 -0400

    Remove props and import copiedReducer

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -2,18 +2,19 @@ import './TextField.scss';
 import CopyIcon from '../../assets/images/icon-copy.svg';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { useSelector, useDispatch } from 'react-redux';
-import { setText } from '../../features/text/textSlice';
+import { setCopied } from '../../features/copied/copiedSlice';
 
-export default function TextField(props) {
+export default function TextField() {
   const text = useSelector((state) => state.text.value);
+  const copied = useSelector((state) => state.copied.value);
   const dispatch = useDispatch();
 
   return (
     <div className='text-field'>
        <span {...text === 'P4$5W0rD!' && {className: 'defaultText'} }>{text}</span>  
-        {props.copied && (<span>COPIED</span>)}
+        {copied && (<span>COPIED</span>)}
         <CopyToClipboard 
-          onCopy={() => props.setCopied(true)}
+          onCopy={() => dispatch(setCopied(true))}
           text={text}
         >
           <img src={CopyIcon} alt="Copy password" />  
