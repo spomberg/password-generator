4a79f65dba0d1eed09f87786bd5190ffa531d5ce
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-28 21:59:37 -0400

    Import textField component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import TextField from './components/TextField/TextField';
 
 function App() {
   return (
     <div className='app'>
       <h1>Password Generator</h1>
+      <TextField/>
     </div>
   );
 }
