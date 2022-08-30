4d6d9ae27002b43b338c57bc1d630d79c2f98d91
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-30 19:47:22 -0400

    Implement generatePassword helper function

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,7 +1,8 @@
 function updateStrength(length, hasUpperCase, hasLowerCase, hasNumbers, hasSymbols) {
   const strength = -10 + length + Number(hasUpperCase) + Number(hasLowerCase) + Number(hasNumbers) + Number(hasSymbols);
 
-  if (strength === -10) return 'N/A';
+  if (strength === -10 || 
+      (!hasUpperCase && !hasLowerCase && !hasNumbers && !hasSymbols) || length < 5) return 'N/A';
   if (strength <= 1) return 'TOO WEAK!';
   if (strength === 2) return 'WEAK';
   if (strength === 3) return 'MEDIUM';
@@ -10,4 +11,26 @@ function updateStrength(length, hasUpperCase, hasLowerCase, hasNumbers, hasSymbo
   return strength;
 }
 
-module.exports = { updateStrength };
+function generatePassword(length, hasUpperCase, hasLowerCase, hasNumbers, hasSymbols) {
+  if (!hasUpperCase && !hasLowerCase && !hasNumbers && !hasSymbols) return null;
+  
+  let result = '';
+  let baseStr = '';
+  const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  const numbers = '0123456789';
+  const symbols = '@%+/!#$?:,(){}[]';
+
+  if (hasUpperCase) baseStr = baseStr.concat(upperCase);
+  if (hasLowerCase) baseStr = baseStr.concat(lowerCase);
+  if (hasNumbers) baseStr = baseStr.concat(numbers);
+  if (hasSymbols) baseStr = baseStr.concat(symbols);
+
+  for (let index = 0; index < length; index++) {
+    result = result.concat(baseStr[Math.floor(Math.random() * baseStr.length)])
+  }
+
+  return result;
+}
+
+module.exports = { updateStrength, generatePassword };
