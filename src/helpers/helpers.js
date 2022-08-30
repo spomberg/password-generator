69f7ac3f3318e544756ef76eca27bdcfcf6505fe
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-30 15:47:20 -0400

    Return strings to display password strength instead of numbers

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,5 +1,13 @@
 function updateStrength(length, hasUpperCase, hasLowerCase, hasNumbers, hasSymbols) {
-  return -10 + length + Number(hasUpperCase) + Number(hasLowerCase) + Number(hasNumbers) + Number(hasSymbols);
+  const strength = -10 + length + Number(hasUpperCase) + Number(hasLowerCase) + Number(hasNumbers) + Number(hasSymbols);
+
+  if (strength === -10) return '';
+  if (strength <= 1) return 'TOO WEAK!';
+  if (strength === 2) return 'WEAK';
+  if (strength === 3) return 'MEDIUM';
+  if (strength >= 4) return 'STRONG';
+  
+  return strength;
 }
 
 module.exports = { updateStrength };
