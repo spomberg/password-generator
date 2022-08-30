20dab4c761cd6971dd18c50cbdeac3bd9cad10f0
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 20:05:29 -0400

    Import settings slices

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import textReducer from './features/text/textSlice';
 import copiedReducer from './features/copied/copiedSlice';
+import lengthReducer from './features/length/lengthSlice';
+import hasUpperCaseReducer from './features/settings/hasUpperCaseSlice';
+import hasLowerCaseReducer from './features/settings/hasLowerCaseSlice';
+import hasNumbersReducer from './features/settings/hasNumbersSlice';
+import hasSymbolsReducer from './features/settings/hasSymbolsSlice';
 
 export default configureStore({
   reducer: {
     text: textReducer,
-    copied: copiedReducer
+    copied: copiedReducer,
+    length: lengthReducer,
+    hasUpperCase: hasUpperCaseReducer,
+    hasLowerCase: hasLowerCaseReducer,
+    hasNumbers: hasNumbersReducer,
+    hasSymbols: hasSymbolsReducer
   },
 });
