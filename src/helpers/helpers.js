6bed95137ad8b76a35deb57416ef2643df0f136e
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-30 18:38:19 -0400

    Return N/A instead of empty string when form is empty

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,7 +1,7 @@
 function updateStrength(length, hasUpperCase, hasLowerCase, hasNumbers, hasSymbols) {
   const strength = -10 + length + Number(hasUpperCase) + Number(hasLowerCase) + Number(hasNumbers) + Number(hasSymbols);
 
-  if (strength === -10) return '';
+  if (strength === -10) return 'N/A';
   if (strength <= 1) return 'TOO WEAK!';
   if (strength === 2) return 'WEAK';
   if (strength === 3) return 'MEDIUM';
