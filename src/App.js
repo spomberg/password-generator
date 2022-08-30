9cc9a2e2c540295e2194ecbb75de5f88b41deeed
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-29 20:05:47 -0400

    Import and add Settings components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import TextField from './components/TextField/TextField';
+import Slider from './components/Slider/Slider';
 import Settings from './components/Settings/Settings';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='app'>
       <h1>Password Generator</h1>
       <TextField />
+      <Slider />
       <Settings />
     </div>
   );
