a02afdb5a718085cc3a0112d55d3b9dd7a8b47c4
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-30 19:46:22 -0400

    Create GenerateButton component files

---
+++ src/components/GenerateButton/GenerateButton.jsx
@@ -0,0 +1,24 @@
+import './GenerateButton.scss';
+import { generatePassword } from '../../helpers/helpers';
+import { useSelector, useDispatch } from 'react-redux';
+import { setText } from '../../features/text/textSlice'
+import RightArrow from '../../assets/images/icon-arrow-right.svg';
+
+export default function GenerateButton() {
+  const length = useSelector((state) => state.length.value);
+  const hasUpperCase = useSelector((state) => state.hasUpperCase.value);
+  const hasLowerCase = useSelector((state) => state.hasLowerCase.value);
+  const hasNumbers = useSelector((state) => state.hasNumbers.value);
+  const hasSymbols = useSelector((state) => state.hasSymbols.value);
+  const dispatch = useDispatch();
+
+  return (
+    <button 
+      className='generate'
+      onClick={() => dispatch(setText(generatePassword(length, hasUpperCase, hasLowerCase, hasNumbers, hasSymbols)))}  
+    >
+      <h3>GENERATE</h3>
+      <img src={RightArrow} alt="Right Arrow" />
+    </button>
+  )
+}
