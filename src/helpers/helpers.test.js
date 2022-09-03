b147225c6513f2e221db4c65a89dad177ea22b0a
Marcos Spomberg <marcosspomberg@gmail.com>
2022-09-03 12:44:49 -0400

    Implement unhappy path test for isPasswordValid

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -11,4 +11,15 @@ describe('isPasswordValid helper function', () => {
 
     expect(input).toBe(true);
    })
- })
+
+  test('should return false when the str does not contain at least a char for each setting', () => { 
+    const pw = '6rNofhEO';
+    const hasUpperCase = true;
+    const hasLowerCase = true;
+    const hasNumbers = true;
+    const hasSymbols = true;
+    const input = isPasswordValid(pw, hasUpperCase, hasLowerCase, hasNumbers, hasSymbols);
+
+    expect(input).toBe(false);
+   })
+ });
